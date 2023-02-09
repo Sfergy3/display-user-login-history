d7dadb495d77a9e9226079a8a7a51097ad4551f9
s3 <stanleyl.fergusoniii@gmail.com>
2023-02-09 17:22:05 +0000

    I am the lord

---
+++ history.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# User login history
+# Stanley L. Ferguson III
+# 8 Feb 2023
+# This is going ot print the user login history to the screen
+
+function login_hist {
+ lastlog 
+ echo "would you like me to do anything else lord" $1
+}
+
+login_hist stanley?
+
+
+# main
+
+# end
+
